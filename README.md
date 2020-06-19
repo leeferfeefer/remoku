eaca837756e1bf2ccbd90e21130d2ae1d71b445d
leeferfeefer <danbfjr@gmail.com>
2020-06-18 20:36:36 -0400

    Added search functionality. Updated Readme

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+Tutorials:   
+https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world   
+https://testdriven.io/blog/developing-a-single-page-app-with-flask-and-vuejs/
+
+
+Credits:
+I did not write the search.service.py file. It was copied from: https://gist.github.com/dankrause/6000248
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
 from app import app
+from flask import Response
+from services.search import discover
+import json
 
-@app.route('/')
-@app.route('/index')
-def index():
-	return "Hello, World!"
+
+@app.route('/search')
+def search():
+    devices = discover("roku:ecp", timeout=10)
+    jsonStr = json.dumps([device.__dict__ for device in devices])
+    return Response(jsonStr, status=200, mimetype='application/json')
