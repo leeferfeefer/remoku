034384336403f559450c8ac5206d5927ac0fcf52
leeferfeefer <danbfjr@gmail.com>
2020-06-18 21:28:42 -0400

    Converted routes to blueprint. Created API directory. Started keypress implementation

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
 
-from app import routes
+def create_app(config_class=None):
+    app = Flask(__name__)
+
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+    # print(app.url_map)
+    return app
--- app/api/api.py
+++ app/api/api.py
@@ -1,11 +1,19 @@
-from app import app
-from flask import Response
+from app.api import bp
+from flask import Response, request
 from services.search import discover
 import json
+import requests
 
 
-@app.route('/search')
+@bp.route('/search', methods=['GET'])
 def search():
     devices = discover("roku:ecp", timeout=10)
     jsonStr = json.dumps([device.__dict__ for device in devices])
     return Response(jsonStr, status=200, mimetype='application/json')
+
+
+@bp.route('/keyPress/<string:key>', methods=['POST'])
+def keyPress():
+    data = request.get_json() or {}
+
+    requests.post()
