0bd07d32f1d3cddf5b8f76ce0dd7cb0926747146
leeferfeefer <danbfjr@gmail.com>
2020-06-20 13:45:20 -0400

    Added ui buttons

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="deep-purple accent-2"
+      dark
+    >
+      <v-spacer></v-spacer>
+      <v-toolbar-title>Remoku</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-content>
+      <Roku/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Roku from './components/Roku';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Roku,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ ui/src/components/Roku.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-container fluid>
+        <v-card
+            class="mx-auto"
+            max-width="500"
+            align="center"
+            justify="center"
+        >
+            <v-row
+                align="center"
+                justify="center"
+                class="ma-4"
+            >
+                <v-btn dark large color="red">
+                    <v-icon dark>fa-power-off</v-icon>
+                </v-btn>        
+            </v-row>
+            <v-row
+                align="center"
+                justify="center"
+                class="ma-4"
+            >
+                <v-btn dark large color="grey lighten-1" class="ml-2 mr-2">
+                    <v-icon dark>fa-long-arrow-alt-left</v-icon>
+                </v-btn>
+                <v-btn dark large color="grey lighten-1" class="ml-2 mr-2">
+                    <v-icon dark>fa-search</v-icon>
+                </v-btn>                
+                <v-btn dark large color="grey lighten-1" class="ml-2 mr-2">
+                    <v-icon dark>fa-asterisk</v-icon>
+                </v-btn>        
+            </v-row>
+            <v-row
+                align="center"
+                justify="center"
+                class="ma-4"
+            >
+                <v-btn dark large color="grey lighten-1" class="ml-2 mr-2">
+                    <v-icon dark>fa-home</v-icon>
+                </v-btn>
+                <v-btn dark large color="deep-purple accent-2" class="ml-2 mr-2">
+                    <v-icon dark>fa-arrow-up</v-icon>
+                </v-btn>                
+                <v-btn dark large color="grey lighten-1" class="ml-2 mr-2">
+                    <v-icon dark>fa-random</v-icon>
+                </v-btn>        
+            </v-row>
+            <v-row
+                align="center"
+                justify="center"
+                class="ma-4"
+            >
+                <v-btn dark large color="deep-purple accent-2" class="ml-2 mr-2">
+                    <v-icon dark>fa-arrow-left</v-icon>
+                </v-btn>
+                <v-btn dark large color="deep-purple accent-2" class="ml-2 mr-2">
+                    <v-icon dark>fa-circle</v-icon>
+                </v-btn>                
+                <v-btn dark large color="deep-purple accent-2" class="ml-2 mr-2">
+                    <v-icon dark>fa-arrow-right</v-icon>
+                </v-btn>        
+            </v-row>
+            <v-row
+                align="center"
+                justify="center"
+                class="ma-4"
+            >
+                <v-btn dark large color="grey lighten-1" class="ml-2 mr-2">
+                    <v-icon dark>fa-volume-down</v-icon>
+                </v-btn>
+                <v-btn dark large color="deep-purple accent-2" class="ml-2 mr-2">
+                    <v-icon dark>fa-arrow-down</v-icon>
+                </v-btn>                
+                <v-btn dark large color="grey lighten-1" class="ml-2 mr-2">
+                    <v-icon dark>fa-volume-up</v-icon>
+                </v-btn>        
+            </v-row>
+            <v-row
+                align="center"
+                justify="center"
+                class="ma-4"
+            >
+                <v-btn dark large color="grey lighten-1" class="ml-2 mr-2">
+                    <v-icon dark>fa-volume-off</v-icon>
+                </v-btn>        
+            </v-row>
+        </v-card>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Roku',
+
+    data: () => ({
+    }),
+  }
+</script>
